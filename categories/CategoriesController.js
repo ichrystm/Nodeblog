ed6737b0dd2c4d43500b9776df8d7ecc5774eb3e
ichrystm <ichrystm@gmail.com>
2020-11-17 18:41:49 -0300

    adding edit and delete category functions

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
       title: title,
       slug: slugify(title)
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/categories");
     })
   }else{
     res.redirect("admin/categories/new");
@@ -27,5 +27,47 @@ router.get("/admin/categories", (req, res) => {
     res.render("./admin/categories/index", {categories: categories});
   })
 });
+
+
+router.post("/categories/delete", (req, res) => {
+  var id = req.body.id;
+  if(id != undefined){
+    if(!isNaN(id)){
+      
+      Category.destroy({
+        where: {id: id}
+      })
+      .then(() => {
+        res.redirect("/admin/categories");
+      });
+
+    }else{
+      res.redirect("/admin/categories");
+    }
+  }else{
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  var id = req.params.id;
+
+  if(isNaN(id)){
+    res.redirect("/admin/categories");
+  }
+
+  Category.findByPk(id).then(categorie => {
+    if(categorie != undefined){
+
+      res.render("./admin/categories/edit");
+
+    }else{
+      res.redirect("/admin/categories");
+    }
+  })
+  .catch(err => {
+    res.redirect("/admin/categories");
+  })
+})
 
 module.exports = router;
