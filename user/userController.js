7acb68af06a43f71838415be82fbc9968910679c
ichrystm <ichrystm@gmail.com>
2020-12-08 10:56:10 -0300

    password hash

--- user/userController.js
+++ user/userController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./user");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin/users", (req,res) => {
   res.send("listagem de usuarios");
@@ -14,7 +15,17 @@ router.post("/users/create", (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
-  res.json({email, password});
-})
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+
+  User.create({
+    email: email,
+    password: hash
+  }).then(() => {
+    res.redirect("/");
+  }).catch((err) => {
+    res.redirect("/");
+  })
+});
 
 module.exports = router;
